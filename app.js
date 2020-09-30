7d676527b0b506573ac6a84afe6d170486b4cbfe
cat - lin <plummorgan@gmail.com>
2020-09-30 12:26:07 -0700

    generated html

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const profileDataArg = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArg);
-
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('=======');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//global variables
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 
-printProfileData(profileDataArg);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});

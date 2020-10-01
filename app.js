af6c600dfccdd236862d0bf68086595b84e56ba8
cat - lin <plummorgan@gmail.com>
2020-10-01 16:39:23 -0700

    added when method to prompt user response

--- app.js
+++ app.js
@@ -33,27 +33,25 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true,
+        },
+        {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
-
-
-// function Burp(sound){
-//     console.log(this);
-// }
-// Burp.name='hiiii';
-// var BURP = function(sound){
-//     console.log(this);
-// }
-// BURP.name='hi';
-
-// var burp = sound => {
-//     console.log(this);
-// }
-// burp.name= 'ok??';
 
 const promptProject = (portfolioData) => {
     if (!portfolioData.projects) {
